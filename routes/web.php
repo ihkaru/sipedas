aec13f0d1e03925f251f8dd56d1a03ab9777fe5a
ihkaru <ihza2karunia@gmail.com>
2024-04-26 17:56:23 +0700

    fix artisan route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ArtisanController;
 use Illuminate\Support\Facades\Route;
 
+Route::get("/run",[ArtisanController::class,"run"]);
 Route::get('/', function () {
     return view('welcome');
 });
