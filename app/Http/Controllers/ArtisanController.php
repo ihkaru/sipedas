99f44936e3e60c1809e4f36aadba8b8451028bad
ihkaru <ihza2karunia@gmail.com>
2024-04-26 17:59:21 +0700

    fix artisan

--- app/Http/Controllers/ArtisanController.php
+++ app/Http/Controllers/ArtisanController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Composer;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Composer;
 use Illuminate\Support\Facades\Artisan;
 
 class ArtisanController extends Controller
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,37 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function responsejson($code,$data,$message = ''){
+        $status = ($code>=200 && $code<300)?true:false;
+        if($message == ''){
+            return response()->json([
+                "status"=>$status,
+                "code"=>$code,
+                "data"=>$data
+            ],$code);
+        }else{
+            return response()->json([
+                "status"=>$status,
+                "code"=>$code,
+                "data"=>$data,
+                "message"=>$message
+            ],$code);
+        }
+    }
+
+    public function responseabort($code,$message){
+        return $this->responsejson($code,["message"=>$message]);
+    }
+
+    public function emptyHandler(String $key){
+        if(request($key)){
+            return request($key);
+        }else{
+            if(request()->exists($key)){
+                return "";
+            }else {
+                return null;
+            }
+        }
+    }
 }
