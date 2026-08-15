eef7e26aa09b28f2db89983c406286c30a16f59c
Ihza <ihkaru@gmail.com>
2026-08-15 15:27:19 +0700

    fix(theme): configure Tailwind CSS v3 preset for Filament v5 custom theme HMR and production build

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -1,7 +1,13 @@
+import preset from '../../../../vendor/filament/support/tailwind.config.preset'
+
 export default {
+    presets: [preset],
     content: [
+        '../../../../app/Filament/**/*.php',
+        '../../../../resources/views/**/*.blade.php',
+        '../../../../vendor/filament/**/*.blade.php',
         './app/Filament/**/*.php',
-        './resources/views/filament/**/*.blade.php',
+        './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
     safelist: [
