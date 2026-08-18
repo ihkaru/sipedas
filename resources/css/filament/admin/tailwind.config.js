038cab8280aa7415f7c1817a30a70a05b0263d8f
Ihza <ihkaru@gmail.com>
2026-08-18 14:45:44 +0700

    fix(theme): remove obsolete filament preset in admin tailwind config and update build manifest

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -1,11 +1,5 @@
-import preset from '../../../../vendor/filament/support/tailwind.config.preset'
-
 export default {
-    presets: [preset],
     content: [
-        '../../../../app/**/*.php',
-        '../../../../resources/views/**/*.blade.php',
-        '../../../../vendor/filament/**/*.blade.php',
         './app/**/*.php',
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
